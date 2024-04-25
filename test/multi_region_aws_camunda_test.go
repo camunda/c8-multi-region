6a450f44f1a3fc3353015a025286621c74ff2243
Langleu <lars.lange@camunda.com>
2024-04-25 15:14:02 +0200

    chore(gha): install tools via asdf

--- test/multi_region_aws_camunda_test.go
+++ test/multi_region_aws_camunda_test.go
@@ -26,6 +26,7 @@ const (
 )
 
 var (
+	// renovate: datasource=helm depName=camunda-platform registryUrl=https://helm.camunda.io
 	remoteChartVersion = helpers.GetEnv("HELM_CHART_VERSION", "8.3.11")
 	globalImageTag     = helpers.GetEnv("GLOBAL_IMAGE_TAG", "")    // allows overwriting the image tag via GHA of every Camunda image
 	clusterName        = helpers.GetEnv("CLUSTER_NAME", "nightly") // allows supplying random cluster name via GHA
