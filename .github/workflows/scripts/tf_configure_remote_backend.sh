f5b9c35cef3bb2a7dab723c6f1ae2b4350c30e91
Langleu <lars.lange@camunda.com>
2025-02-03 13:08:08 +0100

    debug

--- .github/workflows/scripts/tf_configure_remote_backend.sh
+++ .github/workflows/scripts/tf_configure_remote_backend.sh
@@ -24,8 +24,13 @@ sed -i '/path/d' "$1"
 # Replace local with s3 backend
 sed -i 's/\"local\"/\"s3\"/g' "$1"
 
-{
-    echo "TF_CLI_ARGS_init=-backend-config='bucket=tf-state-multi-reg' -backend-config='key=state/$CLUSTER_NAME/terraform.tfstate' -backend-config='region=eu-central-1' -backend-config='encrypt=true'"
-    echo "TF_CLI_ARGS=-backend-config='bucket=tf-state-multi-reg' -backend-config='key=state/$CLUSTER_NAME/terraform.tfstate' -backend-config='region=eu-central-1' -backend-config='encrypt=true'"
-    echo "TF_LOG=DEBUG"
-} >> "$GITHUB_ENV"
+cat <<EOF > ~/.terraformrc
+cli {
+  args = [
+    "-backend-config=bucket=tf-state-multi-reg",
+    "-backend-config=key=state/\${CLUSTER_NAME}/terraform.tfstate",
+    "-backend-config=region=eu-central-1",
+    "-backend-config=encrypt=true"
+  ]
+}
+EOF
