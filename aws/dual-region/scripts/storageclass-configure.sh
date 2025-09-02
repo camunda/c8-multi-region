ac3111f1e9ab680cad19ec9e35694a2e91bb48e4
Langleu <lars.lange@camunda.com>
2025-09-02 13:53:42 +0200

    fix: wrong path

--- aws/dual-region/scripts/storageclass-configure.sh
+++ aws/dual-region/scripts/storageclass-configure.sh
@@ -5,5 +5,5 @@ kubectl --context "$CLUSTER_0" patch storageclass gp2 \
 kubectl --context "$CLUSTER_1" patch storageclass gp2 \
   -p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 
-kubectl --context "$CLUSTER_0" apply -f "../storage-class.yml"
-kubectl --context "$CLUSTER_1" apply -f "../storage-class.yml"
+kubectl --context "$CLUSTER_0" apply -f "../kubernetes/storage-class.yml"
+kubectl --context "$CLUSTER_1" apply -f "../kubernetes/storage-class.yml"
