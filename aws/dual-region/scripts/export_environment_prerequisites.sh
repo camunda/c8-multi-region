fda98c4a3ab89055f5fdbe3cb6a9e653f89a041c
Lars Lange <9141483+Langleu@users.noreply.github.com>
2025-10-14 17:24:53 +0200

    chore: 8.8 required adjustments (#779)
    Co-authored-by: Leo <153937047+leiicamundi@users.noreply.github.com>

--- aws/dual-region/scripts/export_environment_prerequisites.sh
+++ aws/dual-region/scripts/export_environment_prerequisites.sh
@@ -25,6 +25,6 @@ export CAMUNDA_NAMESPACE_1=camunda-paris
 # The Helm release name used for installing Camunda 8 in both Kubernetes clusters
 export CAMUNDA_RELEASE_NAME=camunda
 
-# renovate: datasource=helm depName=camunda-platform registryUrl=https://helm.camunda.io
-export HELM_CHART_VERSION=12.6.1
+# renovate: datasource=helm depName=camunda-platform registryUrl=https://helm.camunda.io versioning=regex:^13(\.(?<minor>\d+))?(\.(?<patch>\d+))?$
+export HELM_CHART_VERSION=13.0.0
 # TODO: [release-duty] before the release, update this!
